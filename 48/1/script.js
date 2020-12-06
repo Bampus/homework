5c8ccb4391bb05141435da0bb543edc915f82de3
Bampus <Hampuswallinder321@gmail.com>
2020-12-06 14:07:55 +0100

    commit f

--- 48/1/script.js
+++ 48/1/script.js
@@ -1,3 +1,6 @@
 // Deklarera en variabel name och ge den värdet av ditt namn
-
+const name = "Hampus"
 // Skriv en for-loop som loggar ut "Hejsan <ditt namn här>!!" 500 gånger
+for(var i = 1; i < 500; i += 1) {
+    console.log("Hejsan " + name +" !!");
+}
--- 48/2/script.js
+++ 48/2/script.js
@@ -2,6 +2,13 @@
 // Som varje iteration (repetition) slumpar ett heltal mellan 0 och 10
 // Om talet blir 6, avsluta loopen
 
+i = Math.floor(Math.random() * 10)
+do {
+  text += i + ' ';
+  i = Math.floor(Math.random() * 10)
+} while (i !== 6)
+  text += i;
+console.log(text);
 
 // Exempel på output:
 // 4
